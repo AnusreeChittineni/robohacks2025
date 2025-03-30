5fc06819031dbb53dd71f3ff78ecf818345095e2
Seth Shoneman <60634819+SethShoneman@users.noreply.github.com>
2025-03-30 07:07:43 -0400

    demo stuff

--- Arduino Code/Motor_Control/Motor_Control.ino
+++ Arduino Code/Motor_Control/Motor_Control.ino
@@ -1,11 +1,5 @@
-int FRwheelPin1 = 4;
-int FRwheelPin2 = 5;
-int FLwheelPin1 = 2;
-int FLwheelPin2 = 3;
-int BRwheelPin1 = 8;
-int BRwheelPin2 = 9;
-int BLwheelPin1 = 6;
-int BLwheelPin2 = 7;
+int WheelPin1 = 8;
+int WheelPin2 = 9;
 
 int GearPin1 = 12;
 int GearPin2 = 13;
@@ -14,14 +8,14 @@ bool commandStarted = false;
 bool onGround = true;
 
 void setup() {
-  Serial.begin(115200);
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB
-  }
-  pinMode(FRwheelPin, OUTPUT);
-  pinMode(FLwheelPin, OUTPUT);
-  pinMode(BRwheelPin, OUTPUT);
-  pinMode(BLwheelPin, OUTPUT);
+//   Serial.begin(115200);
+//   while (!Serial) {
+//     ; // wait for serial port to connect. Needed for native USB
+//   }
+  pinMode(WheelPin1, OUTPUT);
+  pinMode(WheelPin2, OUTPUT);
+  pinMode(GearPin1, OUTPUT);
+  pinMode(GearPin2, OUTPUT);
 }
 
 void loop() {
@@ -32,142 +26,73 @@ void loop() {
     G = on ground
     R = on rail
     W = move forward
-    A = move right
-    B = move backward
-    L = move left
-    H = rotate counterclockwise
-    J = rotate clockwise
+    A = turn left
+    F = turn right
     U = move up
     D = move down
     E = command ended
   */
-  if (Serial.available() > 0) {
-    comm = Serial.read();
+  digitalWrite(WheelPin1, HIGH);
+  digitalWrite(WheelPin2, LOW);
 
-    // determine if within an active command sequence
-    if (!commandStarted & (comm == 'S')) {
-        commandStarted = true;
-    } else if (commandStarted & (comm == 'E')) {
-        commandStarted = false;
-    }
+  delay(1000);
 
-    if (commandStarted) {
-        // determine if on the ground or rail
-        if (comm == 'R') {
-            onGround = false;
-        } else if (comm == 'G') {
-            onGround = true;
-        }
+  digitalWrite(WheelPin1, HIGH);
+  digitalWrite(WheelPin2, HIGH);
 
-        if (onGround) {
-            if (comm == 'W') {
-                goForward();
-            } else if (comm = 'A') {
-                goRight();
-            } else if (comm = 'B') {
-                goBackward();
-            } else if (comm = 'L') {
-                goLeft();
-            } else if (comm = 'H') {
-                rotateCCW();
-            } else if (comm = 'J') {
-                rotateCW();
-            }
-        } else {
-            if (comm == 'U') {
-                goUp();
-            } else if (comm == 'D') {
-                goDown();
-            }
-        }
-    } else { // if no command is being sent, stop all motion
-        breakAll();
-    }
+  delay(1000);
+
+  digitalWrite(GearPin1, HIGH);
+  digitalWrite(GearPin2, LOW);
+
+  delay(2500);
+
+  digitalWrite(GearPin1, HIGH);
+  digitalWrite(GearPin2, HIGH);
+
+  while (true) {
+    continue;
+  }
 }
+//                 digitalWrite(WheelPin2, LOW);
+//                 digitalWrite(WheelPin2, LOW);
+//   if (Serial.available() > 0) {
+//     comm = Serial.read();
 
-void goForward() {
-    digitalWrite(FRwheelPin1, HIGH);
-    digitalWrite(FRwheelPin2, LOW);
+//     // determine if within an active command sequence
+//     if (!commandStarted & (comm == 'S')) {
+//         commandStarted = true;
+//     } else if (commandStarted & (comm == 'E')) {
+//         commandStarted = false;
+//     }
 
-    digitalWrite(FLwheelPin1, HIGH);
-    digitalWrite(FLwheelPin2, LOW);
+//     if (commandStarted) {
+//         // determine if on the ground or rail
+//         if (comm == 'R') {
+//             onGround = false;
+//         } else if (comm == 'G') {
+//             onGround = true;
+//         }
 
-    digitalWrite(BRwheelPin1, HIGH);
-    digitalWrite(BRwheelPin2, LOW);
-
-    digitalWrite(BLwheelPin1, HIGH);
-    digitalWrite(BLwheelPin2, LOW);
-}
-
-void goBackward() {
-    digitalWrite(FRwheelPin1, LOW);
-    digitalWrite(FRwheelPin2, HIGH);
-
-    digitalWrite(FLwheelPin1, LOW);
-    digitalWrite(FLwheelPin2, HIGH);
-
-    digitalWrite(BRwheelPin1, LOW);
-    digitalWrite(BRwheelPin2, HIGH);
-
-    digitalWrite(BLwheelPin1, LOW);
-    digitalWrite(BLwheelPin2, HIGH);
-}
-
-void goLeft() {
-    digitalWrite(FRwheelPin1, HIGH);
-    digitalWrite(FRwheelPin2, LOW);
-
-    digitalWrite(FLwheelPin1, LOW);
-    digitalWrite(FLwheelPin2, HIGH);
-
-    digitalWrite(BRwheelPin1, LOW);
-    digitalWrite(BRwheelPin2, HIGH);
-
-    digitalWrite(BLwheelPin1, HIGH);
-    digitalWrite(BLwheelPin2, LOW);
-}
-
-void goRight() {
-    digitalWrite(FRwheelPin1, LOW);
-    digitalWrite(FRwheelPin2, HIGH);
-
-    digitalWrite(FLwheelPin1, HIGH);
-    digitalWrite(FLwheelPin2, LOW);
-
-    digitalWrite(BRwheelPin1, HIGH);
-    digitalWrite(BRwheelPin2, LOW);
-
-    digitalWrite(BLwheelPin1, LOW);
-    digitalWrite(BLwheelPin2, HIGH);
-}
-
-void rotateCW() {
-    digitalWrite(FRwheelPin1, HIGH);
-    digitalWrite(FRwheelPin2, LOW);
-
-    digitalWrite(FLwheelPin1, LOW);
-    digitalWrite(FLwheelPin2, HIGH);
-
-    digitalWrite(BRwheelPin1, HIGH);
-    digitalWrite(BRwheelPin2, LOW);
-
-    digitalWrite(BLwheelPin1, LOW);
-    digitalWrite(BLwheelPin2, HIGH);
-}
-
-void rotateCCW() {
-    digitalWrite(FRwheelPin1, LOW);
-    digitalWrite(FRwheelPin2, HIGH);
-
-    digitalWrite(FLwheelPin1, HIGH);
-    digitalWrite(FLwheelPin2, LOW);
-
-    digitalWrite(BRwheelPin1, LOW);
-    digitalWrite(BRwheelPin2, HIGH);
-
-    digitalWrite(BLwheelPin1, HIGH);
-    digitalWrite(BLwheelPin2, LOW);
-}
+//         if (onGround) {
+//             if (comm == 'A') {
+//                 digitalWrite(WheelPin1, HIGH);
+//                 digitalWrite(WheelPin2, LOW);
+//             } else if (comm = 'F') {
+//                 digitalWrite(WheelPin1, LOW);
+//                 digitalWrite(WheelPin2, HIGH);
+//             }
+//         } else {
+//             if (comm == 'U') {
+//                 goUp();
+//             } else if (comm == 'D') {
+//                 goDown();
+//             }
+//         }
+//     } else { // if no command is being sent, stop all motion
+//         breakAll();
+//     }
+// }
 
 void goUp() {
     digitalWrite(GearPin1, HIGH);
@@ -180,17 +105,8 @@ void goDown() {
 }
 
 void breakAll() {
-    digitalWrite(FRwheelPin1, HIGH);
-    digitalWrite(FRwheelPin2, HIGH);
-
-    digitalWrite(FLwheelPin1, HIGH);
-    digitalWrite(FLwheelPin2, HIGH);
-
-    digitalWrite(BRwheelPin1, HIGH);
-    digitalWrite(BRwheelPin2, HIGH);
-
-    digitalWrite(BLwheelPin1, HIGH);
-    digitalWrite(BLwheelPin2, HIGH);
+    digitalWrite(WheelPin1, HIGH);
+    digitalWrite(WheelPin2, HIGH);
     
     digitalWrite(GearPin1, HIGH);
     digitalWrite(GearPin2, HIGH);
